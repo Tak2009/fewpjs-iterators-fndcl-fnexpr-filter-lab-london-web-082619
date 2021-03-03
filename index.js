dc1f0152bc2fd64928e62d78c2487f2010a459e5
Takeshi Kimoto <lnuk2009jp@gmail.com>
2021-03-03 22:23:58 +0000

    commit tree a96403d2e6ecd6b7baf0c1e745317fb92cbbe990

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 //case sensitive
 
-const drivers = ["Takeshi", "david", "Paul", "una", "Dolice", "dave"];
+const drivers = ["Takeshi", "david", "Paul", "una", "Dolice", "dave","takeharu"];
 const driver = "Una"
 const driverLtrs = "da"
-const driverObjs = [{name: "Takeshi", age: 1},{name: "Mikasa", age: 2},{name: "Una", age: 3}]
+const driverObjs = [{name: "Takeshi", age: 1},{name: "Mikasa", age: 2},{name: "Una", age: 3},{name: "Tanako", age: 4}]
 
 const findMatching = (drivers, driver) => {
     let matches = drivers.filter(d => {
@@ -19,7 +19,7 @@ findMatching(drivers, driver);
 
 const fuzzyMatch = (drivers, driverIni) => {
     let matches = drivers.filter(d => {
-        return d.slice(0,2) === driverIni
+        return d.slice(0,2).toLowerCase() === driverIni.toLowerCase()
     })
     console.log(matches)
     return matches
@@ -29,7 +29,7 @@ fuzzyMatch(drivers, driverLtrs);
 
 const matchName = (driverObjs, driver) => {
     let matches = driverObjs.filter(d => {
-        return d.name === driver
+        return d.name.slice(0,2).toLowerCase() === driver.toLowerCase()
     })
     console.log(matches)
     return matches
